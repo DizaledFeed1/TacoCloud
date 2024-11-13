71045fbfb7afec800d1a0805cd5f51a96a947d2f
Dizaled <silifslikirill@gmail.com>
2024-11-13 20:49:05 +0700

    9.1.2-254-'13|11|24'(пофиксить бак с сервером авторизации, дальше не идти)

---
+++ src/main/java/com/example/tacocloud/messaging/JmsOrderMessagingService.java
@@ -0,0 +1,26 @@
+package com.example.tacocloud.messaging;
+
+import com.example.tacocloud.TacoOrder;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JmsOrderMessagingService implements OrderMessagingService {
+    private JmsTemplate jms;
+
+    @Autowired
+    public JmsOrderMessagingService(JmsTemplate jms) {
+        this.jms = jms;
+    }
+
+    @Override
+    public void sendOrder(TacoOrder order) {
+        jms.send(session -> session.createObjectMessage(order));
+    }
+}
---
+++ src/main/java/com/example/tacocloud/web/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.tacocloud.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";  // возвращаем имя страницы login.html
+    }
+}
+
